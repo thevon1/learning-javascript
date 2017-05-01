8a4de3d152ec04c127624589e499b8a4dfcbd9e3
jsdevel <js.developer.undefined@gmail.com>
2017-04-30 18:20:43 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# learning-javascript
+> A set of handls on lessons to learn javascript.
+
+The lessons can be found in the `lessons/` directory.  It is recommended that you start at lesson-1 and proceed from there.
---
+++ lessons/lesson-1-comments-whitespace-programs.js
@@ -0,0 +1,55 @@
+// This is a single line comment.  It is called single line because it cannot be broken up onto multiple lines.  That is why this comment is getting so long.  The author is unable to break it up onto multiple lines.  :/
+
+// To get around this,  you can split
+// a single line comment across several single line comments.
+// Single line comments start with "//".  They end when the line ends.
+
+/*
+Multi line comments start with "/*".  They can
+be broken up across multiple lines.
+To end a multi line comment you terminate it with an asterisk followed by a forward
+slash.  I cannot type that in quotes otherwise I would end this comment :D
+*/
+
+// This file represents a javascript program.  A javascript program is simply a
+// collection of unicode characters.  If you're wondering what unicode is, google it.
+// Characters are represented by keys on your keyboard.  "a" represents the character
+// "a" and so forth.  Computers operate on electrical pulses traveling
+// through various circuits.  Processors process these electrical pulses.  These
+// pulses can be thought of as ones and zeros e.g. 101100.  The presence of
+// electrical pulse can be thought of as a 1.  The lack of electrical pulse can be
+// thought of as a 0.
+//
+// Characters are represented by ones and zeros.  We type "a" on our keyboard, but
+// our computer sees something like 1101010101.  It's obviously more complicated
+// than that :).  Unicode is simply an encoding of characters.  In Unicode "a" has
+// a distinct representation of ones and zeros i.e. binary.  Binary is the only
+// numeric representation that traditional computers understand because a digit is
+// either on or off.  We are straying from our intended lesson but you can see that
+// a javascript program is nothing more than a set of characters.
+//
+// This program doesn't do anything useful.  Comments are ignored by javascript run
+// times entirely.  White space is also ignored when it does not appear inside a
+// string.  We'll cover strings in a bit.
+//
+// White space is represented by your space key, your enter key, tab key, etc.
+// Though you don't necessarily see white space when viewing a javascript program, it
+// is there nonetheless and is seen as a character to the javascript run time.
+//
+// A javascript run time is a piece of software that runs javascript.  The most
+// common form of a javascript run time is a web browser.
+//
+// When learning javascript (or any programming language for that matter), it is
+// important to have a javascript run time available so you can run examples and
+// test your knowledge.  This author recommends google chrome as a javascript run
+// time to learn javascript.  Chrome has a developer tools console that allows you
+// to run javascript.  On windows, you can access the javascript console by typing
+// ctrl+shift+j.  On a mac, you can access the javascript console by typing
+// cmd+opt+j.
+//
+// Before continuing to other lessons make sure you can access the javascript console
+// in google chrome.  To very that you're accessing it correctly, type the following
+// into the console and press enter: alert("Hello from the lesson!");
+
+// If it ran properly, then you should have seen an alert window appear.  Once you
+// see the alert window you can move on.
